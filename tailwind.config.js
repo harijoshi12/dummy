5686f9a8ce46fd00d3b57b22dee2dc939f39ed71
Shivangi Gupta <gshivangi106@gmail.com>
2025-03-12 14:32:44 +0530

    updated code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/app/**/*.{js,ts,jsx,tsx}",
-      "./src/components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          primary: "#193f90",
-          secondary: "#ffffff",
-          // Add more if needed
-        },
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#193f90",
+        secondary: "#ffffff",
+        // Add more if needed
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
